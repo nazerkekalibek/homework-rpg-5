6dc771c7f25fe63ed583188c116e0a2b850b5c53
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-03-15 23:01:26 +0500

    Added DungeonFacade

--- src/com/narxoz/rpg/facade/DungeonFacade.java
+++ src/com/narxoz/rpg/facade/DungeonFacade.java
@@ -15,14 +15,17 @@ public class DungeonFacade {
     }
 
     public AdventureResult runAdventure(HeroProfile hero, BossEnemy boss, AttackAction action) {
-        // TODO: Coordinate subsystem calls in a clean order.
-        // Suggested flow:
-        // 1) preparation
-        // 2) battle
-        // 3) reward
-        AdventureResult result = battleService.battle(hero, boss, action);
+        AdventureResult result =new AdventureResult();
         String preparationSummary = preparationService.prepare(hero, boss, action);
         result.addLine(preparationSummary);
+
+        AdventureResult battleResult=battleService.battle(hero, boss, action);
+        result.setWinner(battleResult.getWinner());
+        result.setRounds(battleResult.getRounds());
+
+        for(String line : battleResult.getLog()){
+            result.addLine(line);
+        }
         result.setReward(rewardService.determineReward(result));
         return result;
     }
