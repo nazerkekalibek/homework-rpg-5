5d6d079b5a4339ce0c41283953a67728745c1ae0
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-03-15 21:37:58 +0500

    Added RewardService

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,14 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
+        if (battleResult == null || battleResult.getWinner() == null) {
+            return "No reward";
         }
-        return "TODO";
+
+        if (battleResult.getWinner().equals("Knight")) {
+            return "100 gold + Epic Sword";
+        }
+
+        return "Better luck next time";
     }
 }
